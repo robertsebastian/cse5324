830a7203fb276a63b6f1cb5fcae7cec4357f2cb7
Robert Sebastian <rasebastian@gmail.com>
2013-10-20 15:07:49 -0500

    Play around with fragment layout. Add search bar.

--- TutorFind/gen/com/team7/tutorfind/R.java
+++ TutorFind/gen/com/team7/tutorfind/R.java
@@ -27,18 +27,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_logout=0x7f0a0006;
-        public static final int action_settings=0x7f0a0005;
+        public static final int action_logout=0x7f0a0009;
+        public static final int action_search=0x7f0a000a;
+        public static final int action_settings=0x7f0a0008;
+        public static final int fragment_edit_profile=0x7f0a0006;
         public static final int login_button=0x7f0a0004;
         public static final int login_field=0x7f0a0001;
+        public static final int main_fragment=0x7f0a0005;
         public static final int password_field=0x7f0a0002;
         public static final int register_button=0x7f0a0003;
         public static final int testing=0x7f0a0000;
+        public static final int textView1=0x7f0a0007;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int fragment_edit_profile=0x7f030003;
+        public static final int fragment_search_results=0x7f030004;
     }
     public static final class menu {
         public static final int display_message=0x7f090000;
@@ -46,6 +52,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_logout=0x7f07000f;
+        public static final int action_search=0x7f070013;
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int button_login=0x7f070004;
--- TutorFind/src/com/team7/tutorfind/MainActivity.java
+++ TutorFind/src/com/team7/tutorfind/MainActivity.java
@@ -4,19 +4,25 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 import android.widget.SpinnerAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+	ActionBar.OnNavigationListener,
+	SearchView.OnQueryTextListener 
+{
 
 	public final static String EXTRA_MESSAGE = "com.team7.tutorfind.MESSAGE";
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);     
         
         // Load navigation list for action bar
         ActionBar bar = getActionBar();
@@ -25,20 +31,46 @@ public class MainActivity extends Activity {
         
         SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.action_list,
                 android.R.layout.simple_spinner_dropdown_item);
-        bar.setListNavigationCallbacks(spinnerAdapter, new ActionBar.OnNavigationListener() {
-        	@Override
-        	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        		//String items[] = getResources().getStringArray(R.array.action_list);
-        		
-        		return true;
-        	}
-        });
+        bar.setListNavigationCallbacks(spinnerAdapter, this);
+        /*
+        FragmentManager fm = getFragmentManager();
+        Fragment defaultFrag = fm.findFragmentById(R.id.fragment_edit_profile);
+        
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.main_fragment, defaultFrag);
+        ft.commit();*/
+        
     }
+	
+	@Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+    	//String items[] = getResources().getStringArray(R.array.action_list);
+        		
+        return true;
+    }
+	
+	@Override
+	public boolean onQueryTextChange(String newText)
+	{
+		Log.d("main", newText);
+		return true;
+	}
+	
+	@Override
+	public boolean onQueryTextSubmit(String newText)
+	{
+		Log.d("main", "Submit" + newText);
+		return true;
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+        
         return true;
     }
     
@@ -49,7 +81,7 @@ public class MainActivity extends Activity {
     		startActivity(new Intent(this, SettingsActivity.class));
     		break;
     	case R.id.action_logout:
-    		// TODO
+    		// TODO: Log out and return to login screen
     		break;
     	default:
     		return super.onOptionsItemSelected(item);
