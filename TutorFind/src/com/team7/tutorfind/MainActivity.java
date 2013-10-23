2939e14fd03a51465d6516369ccf64bdfa1c4f24
Robert Sebastian <rasebastian@gmail.com>
2013-10-22 20:51:09 -0500

    Add basic fragment transition code as template

--- TutorFind/src/com/team7/tutorfind/MainActivity.java
+++ TutorFind/src/com/team7/tutorfind/MainActivity.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.FragmentManager;
+import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
@@ -37,21 +37,24 @@ public class MainActivity extends Activity implements
         SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.action_list,
                 android.R.layout.simple_spinner_dropdown_item);
         bar.setListNavigationCallbacks(spinnerAdapter, this);
-        
-        /*
-        FragmentManager fm = getFragmentManager();
-        Fragment defaultFrag = fm.findFragmentById(R.id.fragment_edit_profile);
-        
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.main_fragment, defaultFrag);
-        ft.commit();*/
-        
     }
 	
 	@Override
     public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-    	//String items[] = getResources().getStringArray(R.array.action_list);
-        		
+		// TODO: Pretty ugly way to determine which item is selected -- probably a better way
+    	String item = getResources().getStringArray(R.array.action_list)[itemPosition];
+    	String my_profile_str = getResources().getString(R.string.action_list_my_profile);
+    	String favorites_str = getResources().getString(R.string.action_list_favorites);
+    	
+    	// Transition fragement_container to the selected fragment
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+        if(item.equals(my_profile_str)) {
+        	ft.replace(R.id.fragment_container, new ProfileEditFragment());
+        } else if(item.equals(favorites_str)) {
+        	ft.replace(R.id.fragment_container, new FavoritesFragment());
+        }
+        ft.commit();
+        
         return true;
     }
 
@@ -74,22 +77,6 @@ public class MainActivity extends Activity implements
 		return true;
 	}
 	
-	protected void handleSearchResults(JSONObject results) throws JSONException {
-		Log.d("main", "Results received: " + results.toString());
-        
-		
-        FragmentManager fm = getFragmentManager();
-        //Fragment searchFrag = fm.findFragmentById(R.id.fragment_search_results);
-        
-        FragmentTransaction ft = fm.beginTransaction();
-        //ft.replace(R.id.fragment_container, new SearchResultsFragment());
-        SearchResultsFragment search = new SearchResultsFragment();
-        ft.replace(R.id.fragment_container, search);
-        ft.commit();
-        fm.executePendingTransactions();
-        search.setText(results.toString());
-	}
-	
 	@Override
 	public void onDatabaseResponse(JSONObject response) {
 		try {
@@ -100,7 +87,10 @@ public class MainActivity extends Activity implements
 				// TODO: Error message
 				// TODO: If invalid session, bump back to login screen
 			} else if(response.getString("action").equals("search")) {
-				handleSearchResults(response);
+				FragmentTransaction ft = getFragmentManager().beginTransaction();
+				Fragment searchFragment = SearchResultsFragment.newInstance(response.getJSONArray("results"));
+				ft.replace(R.id.fragment_container, searchFragment);
+				ft.commit();     				
 			}
 		} catch(JSONException e) {
 			Log.e("main", e.toString());
--- TutorFind/src/com/team7/tutorfind/SearchResultsFragment.java
+++ TutorFind/src/com/team7/tutorfind/SearchResultsFragment.java
@@ -1,20 +1,40 @@
 package com.team7.tutorfind;
 
+import org.json.JSONArray;
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class SearchResultsFragment extends Fragment {
+	static SearchResultsFragment newInstance(JSONArray results) {
+		SearchResultsFragment f = new SearchResultsFragment();
+		
+		// Add results argument
+		Bundle args = new Bundle();
+		args.putString("results", results.toString());
+		f.setArguments(args);
+		
+		return f;
+	}
+	
 	@Override
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_search_results, container, false);
 	}
 	
-	public void setText(String text) {
+	@Override
+	public void onViewCreated (View view, Bundle savedInstanceState) {
 		TextView textView = (TextView)getView().findViewById(R.id.test_search_results);
-		textView.setText(text);
+		textView.setText(getArguments().getString("results"));
+	}
+	
+	public void onDestroy() {
+		Log.d("search", "Search fragment destroyed");
+		super.onDestroy();
 	}
 }
