7a604cff39d5b939c99a412e12389ff9a11e161d
Robert Sebastian <rasebastian@gmail.com>
2013-10-27 20:05:45 -0500

    Add progress dialog for database requests

--- TutorFind/src/com/team7/tutorfind/DatabaseRequest.java
+++ TutorFind/src/com/team7/tutorfind/DatabaseRequest.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -17,6 +18,8 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class DatabaseRequest extends AsyncTask<JSONObject, Void, JSONObject> {
+	public static final String TAG = "database_request";
+	
 	public interface Listener {
 		public void onDatabaseResponse(JSONObject response);
 	}
@@ -25,9 +28,12 @@ public class DatabaseRequest extends AsyncTask<JSONObject, Void, JSONObject> {
 	protected String mAddress;
 	protected String mSessionId;
 	protected int mPort;
+	protected Context mContext;
+	protected ProgressDialog mProgress;
 	
 	public DatabaseRequest(JSONObject request, Listener listener, Context context) {
 		mListener = listener;
+		mContext = context;
 		
 		// Read address and port from preferences
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
@@ -43,6 +49,17 @@ public class DatabaseRequest extends AsyncTask<JSONObject, Void, JSONObject> {
 		execute(request);
 	}
 	
+	@Override
+	protected void onPreExecute() {
+		// Show progress dialog while request is in progress
+		mProgress = new ProgressDialog(mContext);
+		mProgress.setTitle("Processing...");
+		mProgress.setMessage("Please wait.");
+		mProgress.setCancelable(false);
+		mProgress.setIndeterminate(true);
+		mProgress.show();
+	}
+	
 	// Make HTTP request to database
 	protected JSONObject doInBackground(JSONObject... requests) {
 		try {
@@ -50,7 +67,7 @@ public class DatabaseRequest extends AsyncTask<JSONObject, Void, JSONObject> {
 			requests[0].put("session_id", mSessionId);
 			
 			// Open POST request
-			URL url = new URL("http", mAddress, mPort, "tutor_find_db.py"); // TODO: Fix hard coded stuff
+			URL url = new URL("http", mAddress, mPort, "tutor_find_db.py");
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 			conn.setDoOutput(true);
 			
@@ -73,17 +90,18 @@ public class DatabaseRequest extends AsyncTask<JSONObject, Void, JSONObject> {
 			
 			return new JSONObject(result.toString());
 		} catch(JSONException e) {
-			Log.e("db", e.toString());
+			Log.e(TAG, e.toString());
 		} catch(MalformedURLException e) {
-			Log.e("db", e.toString());
+			Log.e(TAG, e.toString());
 		} catch(IOException e) {
-			Log.e("db", e.toString());
+			Log.e(TAG, e.toString());
 		}
 		return null;
 	}
 	
 	// Notify listener of response
 	protected void onPostExecute(JSONObject response) {
+		mProgress.dismiss();
 		mListener.onDatabaseResponse(response);
 	}
 }
